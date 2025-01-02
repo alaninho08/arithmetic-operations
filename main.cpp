d6c7aae79acbbbdb4ac7362f7cc92709279a5dd2
alaninho08 <pythagore2007@gmail.com>
2025-01-02 18:01:24 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,70 +1,52 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int main()
-
-{
-    int students = 40;
-
-    // Arithmetic operations = return the result of a specific arithmetic operation (+, -, *, /)
-    //Addition
-    students = students + 1;
-    students+=1;
-    students++;
-
-    // Soustraction
-    students = students - 1;
-    students-= 1;
-    students--;
-
-    //Multiplication
-    students = 40 * 2;
-    students*=2;
-
-    //Division
-    students = 40/2;
-    students/=2;
-
-    //division part.2
-    students = 40/3;
-    students/=3;
-    /* In this case, need to change fundamental from int to double or short*/
-
-// int remainder = students % 6;
-    //cout << remainder
-    int studentsSecond = 6 - (5 + 4) * 3 / 2;
-    cout << studentsSecond;
-
-    return 0;
-}
-
-
-// More detailed arithmetic operations
-#include <iostream>
-using namespace std;
-
-int sumNumbers(int firstValue, int secondValue)
-{
-    int result = firstValue * secondValue;
-    return result;
-}
 int main () {
+    char op;
+    double num1;
+    double num2;
+    double result;
 
-    int firstNumber = 13;
-    int secondNumber = 25;
-    int total = firstNumber * secondNumber;
-    cout << firstNumber << "\n"; // 13
-    cout << secondNumber << "\n"; // 25
-    cout << total << "\n"; //38
+    cout << "***************** CALCULATOR **********************************************" << endl <<
+        "Enter either (+, -, *, /):";
+    cin >> op;
 
+    cout << "Enter number 1: ";
+    cin >>  num1;
 
-    total = sumNumbers(33,46); // 70
-    cout << total << "\n";
+    cout << "Enter number 2: ";
+    cin >> num2;
 
-    total = sumNumbers(45,55); // 100
-    cout << total << "\n";
+    switch (op) {
+        case '+':
+            result = num1 + num2;
+        cout << "result:" << result << endl;
+        break;
 
-    cout << sumNumbers(47, 58) << "\n";
+        case '-':
+            result = num1-num2;
+        cout << "result:" << result << endl;
+
+        break;
+
+        case '*':
+            result = num1*num2;
+        cout << "result:" << result << endl;
+
+        break;
+
+        case '/':
+            result = num1/num2;
+        cout << "result:" << result << endl;
+
+        break;
+
+        default:
+            cout << " That wasn't a valid response" << endl;;
+        break;
+    }
+    cout << "*************************************************";
 
     return 0;
 }
